67365fb12d5bdfac19866d2f3bf45728d24c61dc
Matthew Stoltenberg <d3matt@gmail.com>
2014-01-20 23:34:59 -0600

    minor fixes

--- src/d3matt/d3matt/settings.py
+++ src/d3matt/d3matt/settings.py
@@ -93,5 +93,5 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), 'templates'),
+    os.path.join(ENV_ROOT, 'templates'),
 )
