60fe24a417fd7c81a45698b13a2fec67688017f1
Matthew Stoltenberg <d3matt@gmail.com>
2014-01-27 11:53:24 -0600

    add md_test and navbar

--- src/d3matt/d3matt/views.py
+++ src/d3matt/d3matt/views.py
@@ -3,10 +3,18 @@ import json
 from django.http import Http404, HttpResponse
 from django.shortcuts import render_to_response
 
+LINKS = [
+    ('frontpage', '/d3matt'),
+    ('md test', '/d3matt/md'),
+]
+
 def request_viewer(request):
     response = HttpResponse(content_type='text/plain; charset=utf-8')
     response.write(repr(request))
     return response
 
 def front(request):
-    return render_to_response('frontpage.html', None)
+    return render_to_response('frontpage.html', {'LINKS': LINKS, 'PAGE': 'frontpage'})
+
+def md_test(request):
+    return render_to_response('md_test.html', {'LINKS': LINKS, 'PAGE': 'md test'})
